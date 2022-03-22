a6eb71bf1df09da14286d265042c59e4ced3c799
Cordero Hernandez <cord.a.hernandez@gmail.com>
2022-03-22 13:36:00 -0700

    Adds animations to ContentView and PageTitleView

--- PizzaApp/PageTitleView.swift
+++ PizzaApp/PageTitleView.swift
@@ -21,7 +21,9 @@ struct PageTitleView: View {
                 .padding(.trailing)
         }
         .overlay(
-            Image(systemName: isDisplayingOrders ?? false ? "chevron.up.square" : "chevron.down.square" )
+            Image(systemName: "chevron.up.square")
+                .rotationEffect(isDisplayingOrders ?? false ? Angle(degrees: 0.0) : Angle(degrees: 180.0))
+                .animation(.easeInOut(duration: 0.5))
                 .font(.title)
                 .padding()
                 .foregroundColor(isDisplayingOrders != nil ? Color("G1") : .clear)
